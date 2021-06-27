c3d31f363cac1b020eb30be86b8495f721c09fb6
sawantansh17 <sawantansh17@gmail.com>
2021-06-27 13:53:42 +0530

    First Commit

---
+++ Main Page/script.js
@@ -0,0 +1,31 @@
+let paddlePos = 0;
+let leftArrow = false;
+let rightArrow = false;
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowLeft") {
+    leftArrow = true;
+  } else if (e.key == "ArrowRight") {
+    rightArrow = true;
+  }
+  movePaddle();
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "ArrowLeft") {
+    leftArrow = false;
+  } else if (e.key == "ArrowRight") {
+    rightArrow = false;
+  }
+  movePaddle();
+});
+
+const paddle = document.querySelector(".paddle").style;
+function movePaddle() {
+  if (rightArrow) {
+    paddlePos += 1;
+  } else if (leftArrow) {
+    paddlePos -= 1;
+  }
+  paddle.left = paddlePos + "%";
+}
